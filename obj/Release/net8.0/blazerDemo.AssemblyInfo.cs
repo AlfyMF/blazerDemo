07c689bc0f05969671eaadf45c12c6ab3cc5982b
AlfyMF <alfy.francis@experionglobal.com>
2024-07-18 14:48:21 +0530

    ui update

--- obj/Release/net8.0/blazerDemo.AssemblyInfo.cs
+++ obj/Release/net8.0/blazerDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazerDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e57ed95e77060246b9b989ed0726fa282650d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a291d3d0081e26ead085e6d488a1bd65f17031")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazerDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazerDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
